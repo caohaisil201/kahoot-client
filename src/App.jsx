058fbef0c165771020d8a44c72a65f414c931491
caohaisil201 <caohaisil@gmail.com>
2022-11-08 22:15:28 +0700

    completed base header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import 'assets/styles/index.scss';
+import Header from 'views/components/Header';
 import Error from 'views/pages/Error';
 
 function App() {
   return (
-    <div className="App">
+    <div id="App">
       <BrowserRouter>
+        <Header /> 
         <Routes>
+          <Route path="/news-feed"/>
+          <Route path="/tutorial"/>
+          <Route path="/sign-in"/>
+          <Route path="/sign-up"/>
           <Route path="/"/>
           <Route path="*" element={<Error/>} />
         </Routes>
