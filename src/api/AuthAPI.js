13447bdd4f37ee775487fe8b998f9e917f57bc76
caohaisil201 <caohaisil@gmail.com>
2022-11-29 21:38:27 +0700

    Add constant variable in utils

--- src/api/AuthAPI.js
+++ src/api/AuthAPI.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-
+import { CONSTANT } from 'utils';
 export const registerUser = async (username, email, password, fullName) => {
   try {
     const response = await axios
