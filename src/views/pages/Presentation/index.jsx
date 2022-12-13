0c695ff7c491502e0c8a6fc7208fbdbb2ac576f4
caohaisil201 <caohaisil@gmail.com>
2022-12-13 21:50:18 +0700

    slide UI

--- src/views/pages/Presentation/index.jsx
+++ src/views/pages/Presentation/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'react-router';
 import Loading from 'views/components/Loading';
@@ -6,12 +6,15 @@ import {
   getPresentationByCodeAPI,
   getSlidesFromPresentCodeAPI,
 } from 'api/PresentationAPI';
+import { DeleteOutlined } from '@ant-design/icons';
+import './style.scss';
 
 //Presentation Edit
 const Presentation = () => {
   const accessToken = sessionStorage.getItem('access_token');
   const { code } = useParams();
   const [title, setTitle] = useState('');
+  const [slides, setSlides] = useState([]);
   const slidesQuery = useQuery({
     queryKey: ['getSlide'],
     queryFn: async () => await getSlidesFromPresentCodeAPI(accessToken, code),
@@ -22,6 +25,18 @@ const Presentation = () => {
     queryFn: async () => await getPresentationByCodeAPI(accessToken, code),
   });
 
+  useEffect(() => {
+    if (presentQuery.data) {
+      setTitle(presentQuery.data.name);
+    }
+  }, [presentQuery.isSuccess]);
+
+  useEffect(() => {
+    if (slidesQuery.data) {
+      setSlides([...slidesQuery.data]);
+    }
+  }, [slidesQuery.isSuccess]);
+
   if (slidesQuery.isLoading && presentQuery.isLoading) {
     return <Loading />;
   }
@@ -30,17 +45,32 @@ const Presentation = () => {
     return <div>Error</div>;
   }
 
-  // console.log(slidesQuery.data);
-  console.log(presentQuery.data);
+  const selectSlide = (slide) => {
+    console.log(slide);
+  };
 
   return (
-    <div className="container pt-6 d-flex flex-column">
-      <div>
+    <div className="presentation-detail container pt-6 d-flex flex-column">
+      <div className="header d-flex justify-end">
         <input value={title} onChange={(e) => setTitle(e.target.value)} />
-				<button className="primary medium">Lưu</button>
+        <button className="primary small">Lưu</button>
       </div>
-      <div>
-        <div className="slide-list"></div>
+      <div className="body mt-4">
+        <div className="slide-list d-flex">
+          {slides.map((slide, index) => {
+            return (
+              <div
+                className="item pa-2"
+                key={index}
+              >
+                <div className="d-flex align-center justify-center " onClick={() => selectSlide(slide)}>{index + 1}</div>
+                <button className="icon">
+                  <DeleteOutlined />
+                </button>
+              </div>
+            );
+          })}
+        </div>
         <div></div>
       </div>
     </div>
